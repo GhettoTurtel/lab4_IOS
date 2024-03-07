e2cfdb3b15dbc10c93534832c5c388a7c1123544
GhettoTurtel <dtansykov@gmail.com>
2024-03-07 08:46:08 +0500

    Initial Commit

---
+++ lab4_IOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lab4_IOS
+//
+//  Created by Damir Tansykov on 07.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
